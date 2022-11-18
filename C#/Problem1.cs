c9b68a665edabfd87e7f614be65d6d540cc2b68f
atreanor <alan.treanor@hushmail.com>
2022-11-18 18:10:41 +0000

    Problem 1 solution - complete

---
+++ C#/Problem1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Problem1
+{
+    public static main()
+    {
+        GetMultiplesOf3or5(1000);
+    }
+
+    static int GetMultiplesOf3or5(int limit)
+    {
+
+        int total = 0;
+
+        for  (int i = 1; i < limit; i++)
+        {
+            if (i % 3.0 == 0 || i % 5.0 == 0)
+            {
+                Console.WriteLine(i + " is a multiple of 3 or 5");
+                total += i;
+                Console.WriteLine("Total is: " + total);
+            }
+        }
+        Console.WriteLine("Sum of natural numbers that are multiple of 3 or 5 is " + total);
+        return total;
+    }
+}
